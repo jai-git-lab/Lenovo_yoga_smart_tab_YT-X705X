76f536a2b664f8f1dfc403db4e17eeb41c985104
Jai Kishan <jai201247@gmail.com>
2023-08-24 21:02:00 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,1 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_X705X.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_X705X-user \
+    omni_X705X-userdebug \
+    omni_X705X-eng
