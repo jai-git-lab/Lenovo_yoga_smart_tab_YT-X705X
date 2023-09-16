6d23749d20dcafab00bf763ae4cf9d892d886aa0
Jai Kishan <jai201247@gmail.com>
2023-09-16 22:05:26 +0530

    Update and rename omni_X705X.mk to twrp_X705X.mk

--- twrp_X705X.mk
+++ twrp_X705X.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X705X device
 $(call inherit-product, device/lenovo/X705X/device.mk)
 
 PRODUCT_DEVICE := X705X
-PRODUCT_NAME := omni_X705X
+PRODUCT_NAME := twrp_X705X
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo X705X
 PRODUCT_MANUFACTURER := lenovo
